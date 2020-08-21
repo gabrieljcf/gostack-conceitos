80867a13486d47227a58cb2869f4ab98c6396da1
Gabriel Nogueira <gabrieljcf@hotmail.com>
2020-08-21 20:53:22 -0300

    working with properties in react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,32 @@ import Header from './components/Header';
 function App() {
     return (
         <>
-            <Header />
-            <Header />
+            <Header title="Homepage">
+                <ul>
+                    <li>Homepage</li>
+                    <li>projects</li>
+                </ul>
+            </Header>
+            <Header title="Projects">
+                <ul>
+                    <li>Homepage</li>
+                    <li>projects</li>
+                    <li>Login</li>
+                </ul>
+            </Header>
         </>
     );
 }
 
 export default App;
 
-//um componente é uma função que retorna lógica, estrutura e estilo
-//sempre iremos criar um componente quando queremos reutilizar código em mais lugares da nossa aplicação
+//uma propriedade é alguma informação que podemos passar
+//de um componente pai para um componente filho
+//ao usar o nosso componente podemos passar para ele uma propriedade
+//essa propriedade são como os atributos do html, porem podemos utiliza-los
+//dentro dos nossos componentes
+/**
+ * dentro de todo componente react nós temos uma propriedade
+ * chamda children, essa propriedade é responsavel por passar para o componente filho
+ * todo o conteúdo que o componente pai colocou dentro dele
+ */
