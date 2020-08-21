3d74174d089eafbdb21ee03a15796ef234ce754a
Gabriel Nogueira <gabrieljcf@hotmail.com>
2020-08-21 19:47:39 -0300

    creating the first component

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Header from './components/Header';
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Header />
+        </>
+    );
+}
+
+export default App;
+
+//um componente é uma função que retorna lógica, estrutura e estilo
+//sempre iremos criar um componente quando queremos reutilizar código em mais lugares da nossa aplicação
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,7 @@
-import { soma } from './soma';
+import React from 'react';
+import { render } from 'react-dom';
 
-console.log(soma(4, 4));
+import App from './App';
+
+//JSX html dentro do javascript (javascript xml)
+render(<App />, document.getElementById('app'))
