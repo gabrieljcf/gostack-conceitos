4975317b5f4bb52fa2e9a8285a6d5685f99f4161
Gabriel Nogueira <gabrieljcf@hotmail.com>
2020-08-21 21:33:04 -0300

    working with state and immutability

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 function App() {
+    const [projects, setProjects] = useState(['Criar Homepage', 'Front-end Web']);
+
+    function handleAddProject() {
+        setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    }
+
     return (
         <>
-            <Header title="Homepage">
-                <ul>
-                    <li>Homepage</li>
-                    <li>projects</li>
-                </ul>
-            </Header>
-            <Header title="Projects">
-                <ul>
-                    <li>Homepage</li>
-                    <li>projects</li>
-                    <li>Login</li>
-                </ul>
-            </Header>
+            <Header title="Homepage" />
+            <ul>
+                {projects.map(project => <li key={project}>{project}</li>)}
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>Adicionar</button>
+
         </>
     );
 }
 
 export default App;
-
-//uma propriedade é alguma informação que podemos passar
-//de um componente pai para um componente filho
-//ao usar o nosso componente podemos passar para ele uma propriedade
-//essa propriedade são como os atributos do html, porem podemos utiliza-los
-//dentro dos nossos componentes
-/**
- * dentro de todo componente react nós temos uma propriedade
- * chamda children, essa propriedade é responsavel por passar para o componente filho
- * todo o conteúdo que o componente pai colocou dentro dele
- */
