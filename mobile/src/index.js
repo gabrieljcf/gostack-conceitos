d3d68b6414b221301f51fe65a7e8c2d90838fa6b
Gabriel Nogueira <gabrieljcf@hotmail.com>
2020-08-26 20:57:27 -0300

    getting data from the api and rendering in screen

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, Text, StyleSheet, StatusBar, FlatList } from 'react-native';
+
+import api from './services/api';
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
+
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello GoStack</Text>
-      </View>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({ item: project }) => (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+      </SafeAreaView>
+
     </>
   );
 }
@@ -16,12 +34,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
-  title: {
+  project: {
     color: '#fff',
-    fontSize: 32,
-    fontWeight: 'bold'
+    fontSize: 20,
   },
 });
